ca13f9889efb0556841b03025bce9c8bbf2748f7
zhudedian <zhudedian@gmail.com>
2017-03-16 18:19:21 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/ider/coolweather/util/Utility.java
+++ app/src/main/java/com/ider/coolweather/util/Utility.java
@@ -16,11 +16,11 @@ import org.json.JSONObject;
 
 public class Utility {
 //    解析和处理服务器返回的省级数据
-    public static boolean handleProvinceResponse(String response) throws JSONException {
-        if(!TextUtils.isEmpty(response)){
-            try{
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
+            try {
                 JSONArray allProvinces = new JSONArray(response);
-                for (int i=0; i<allProvinces.length(); i++){
+                for (int i = 0; i < allProvinces.length(); i++) {
                     JSONObject provinceObject = allProvinces.getJSONObject(i);
                     Province province = new Province();
                     province.setProvinceName(provinceObject.getString("name"));
@@ -28,7 +28,7 @@ public class Utility {
                     province.save();
                 }
                 return true;
-            }catch (JSONException e){
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
@@ -41,7 +41,7 @@ public class Utility {
                 JSONArray allCities = new JSONArray(response);
                 for(int i= 0;i<allCities.length();i++){
                     JSONObject cityObject = allCities.getJSONObject(i);
-                    City city = new City():
+                    City city = new City();
                     city.setCityName(cityObject.getString("name"));
                     city.setCityCode(cityObject.getInt("id"));
                     city.setProvinceId(provinceId);
@@ -55,7 +55,7 @@ public class Utility {
         return false;
     }
 //    解析和处理服务器返回的县级数据
-    public static boolean handleCountyResponde(String response, int cityId){
+    public static boolean handleCountyResponse(String response, int cityId){
         if(!TextUtils.isEmpty(response)){
             try{
                 JSONArray allCounties =new JSONArray(response);
